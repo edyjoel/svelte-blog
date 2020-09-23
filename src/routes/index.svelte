77da2a554984ac91d7eed0a718fdc9c01885385d
Edy <edyxicon@gmail.com>
2020-09-23 07:30:57 -0600

    SEO

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,6 +22,24 @@
 
 <svelte:head>
 	<title>Edy Joel</title>
+	<meta name="description" content="En este blog encontras términos relacionados a Microeconomía." />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@edyjoelx" />
+	<meta name="twitter:creator" content="@edyjoelx" />
+	<meta name="twitter:title" content="Blog Microeconómico" />
+	<meta name="twitter:description" content="En este blog encontras términos relacionados a Microeconomía." />
+	<meta name="twitter:image" content="https://arepa.s3.amazonaws.com/blogpost-cover.png">
+
+	
+	<meta name="og:title" content="Blog Microeconómico">
+	<meta name="og:description" content="En este blog encontras términos relacionados a Microeconomía.">
+	<meta name="og:image" content="https://arepa.s3.amazonaws.com/blogpost-cover.png">
+	<meta name="og:url" content="https://diccionarioedy.netlify.app" />
+	<meta name="og:site_name" content="Blog Microeconómico">
+	<meta name="og:locale" content="es_ES" />
+	<meta name="og:type" content="article">
+	<meta name="fb:app_id" content="">
+	
 </svelte:head>
 
 <div class="Home">
